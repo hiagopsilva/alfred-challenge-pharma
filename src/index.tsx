3de9cf09de904e08f9cff51abe664676153f7360
Hiago Prates <hiagohps360@gmail.com>
2022-08-29 19:36:33 -0300

    feat: logo

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+// import { Container } from './styles';
+
+const src: React.FC = () => (
+  <View>
+    <Text>Hello World</Text>
+  </View>
+);
+
+export default src;
